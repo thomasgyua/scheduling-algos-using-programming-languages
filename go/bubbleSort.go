4749e6115ea2dc0479635e223af6c7d2a5bef04f
Parth Bansal <33196466+parthbansal05@users.noreply.github.com>
2022-10-10 01:00:02 +0530

    added sorting algos

---
+++ go/bubbleSort.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+    var n = []int{1, 39, 2, 9, 7, 54, 11}
+
+    var isDone = false
+
+    for !isDone {
+        isDone = true
+        var i = 0
+        for i < len(n) - 1 {
+            if n[i] > n[i + 1] {
+                n[i], n[i + 1] = n[i + 1], n[i]
+                isDone = false
+            }
+            i++
+        }
+    }
+
+    fmt.Println(n)
+}
